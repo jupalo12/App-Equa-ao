2a4de759a944140a28f6122ea76eb53d2bd03df8
Juan Pablo <juam.euzin@gmail.com>
2021-01-15 15:56:39 -0300

    Init

---
+++ js/script.js
@@ -0,0 +1,60 @@
+function equacaofinal(a, b, c) {
+        modal.style.display = "block";
+       
+      
+    a = parseInt(document.getElementById("a").value);
+
+    b = parseInt(document.getElementById("b").value);
+
+    c = parseInt(document.getElementById("c").value)
+
+    if (delta(a, b, c) > 0) {
+        document.getElementById("resul").innerHTML = "O Valor de x1 é: " + x1(a, b, delta(a, b, c))
+        document.getElementById("val b").innerHTML = "O Valor de x2 é: " + x2(a, b, delta(a, b, c))
+        document.getElementById("val h").innerHTML = "O Valor de delta é: " + delta(a,b,c)
+    } else {
+        alert("O delta é negativo ou o os valores não foram definidos")
+    }
+
+    function delta(a, b, c) {
+        return (b ** 2) - (4 * a * c);
+
+    }
+
+    function x1(a, b, valordelta) {
+        return (-b + Math.sqrt(valordelta)) / (2 * a)
+    }
+
+    function x2(a, b, valordelta) {
+        return (-b - Math.sqrt(valordelta)) / (2 * a)
+
+    }
+
+}
+
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("myBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
+
+
+
+
